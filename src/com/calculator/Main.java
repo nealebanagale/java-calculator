db6a0ee031d10624f75edc9cb98616616cce8638
nealebanagale <nealebanagale@yahoo.com>
2021-05-28 20:57:43 +0800

    migrated to GUI

--- src/com/calculator/Main.java
+++ src/com/calculator/Main.java
@@ -1,76 +1,186 @@
 package com.calculator;
 
+import sun.rmi.runtime.Log;
+
 import java.util.Scanner;
 import java.util.ArrayList;
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-    public static void main(String[] args) {
-        // input/output
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to Calculator");
+public class Main implements ActionListener {
+    JFrame frame;
+    JTextField textField;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[9];
+    JButton addButton,subButton,mulButton,divButton;
+    JButton decButton, equButton, delButton, clrButton, negButton;
+    JPanel panel;
+    Font myFont = new Font("Arial", Font.BOLD,30);
+    double num1 = 0;
+    double num2 = 0;
+    double result = 0;
+    char operator; // primitive
 
-        // Array list
-        ArrayList<String> operations = new ArrayList<>();
-        operations.add("addition");
-        operations.add("subtraction");
-        operations.add("multiplication");
-        operations.add("division");
-        System.out.print("Please enter operation: ");
-        System.out.println(operations.toString());
-        String operation = sc.next();
+    // constructor
+    Main() {
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // close program
+        frame.setSize(420, 550);
+        frame.setLayout(null);
 
-        // control flow
-        if (operations.contains(operation)) {
-            int result = 0;
-            System.out.print("Enter first number: ");
-            int a = sc.nextInt();
-            System.out.print("Enter second number: ");
-            int b = sc.nextInt();
+        // text field
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);
+        textField.setFont(myFont);
+        textField.setEditable(false);
 
-            switch(operation) {
-                case "addition":
-                    result = a + b;
-                    break;
-                case "subtraction":
-                    result = a - b;
-                    break;
-                case "multiplication":
-                    result = a * b;
-                    break;
-                case "division":
-                    result = a / b;
-                    break;
-            }
+        // Buttons
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Del");
+        clrButton = new JButton("Clr");
+        negButton = new JButton("(-)");
 
-            System.out.println("Result: " + result);
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = equButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+        functionButtons[8] = negButton;
 
-            // Arrays
-            int[] commonEven = new int[] {2, 4, 10};
-            int[] commonOdd = {3, 7};
-
-            // control flow for-loop
-            for (int i = 0; i < commonOdd.length; i++) {
-                if (commonOdd[i] == result) {
-                    System.out.println(result + " is a common odd number.");
-                }
-            }
-
-            for (int element : commonEven) {
-                if (element == result) {
-                    System.out.println(String.format("%s is a common even number.", result));
-                }
-            }
-        } else {
-            System.out.println("Invalid operation.");
-            return;
+        for (int i = 0; i < 9; i++) {
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false);
         }
 
+        for (int i = 0; i < 10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(myFont);
+            numberButtons[i].setFocusable(false);
+        }
 
+        negButton.setBounds(50,430,100,50);
+        delButton.setBounds(150, 430, 100,50);
+        clrButton.setBounds(250, 430, 100,50);
+
+
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300,300);
+        panel.setLayout(new GridLayout(4,4, 10, 10));
+
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButton);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(mulButton);
+        panel.add(decButton);
+        panel.add(numberButtons[0]);
+        panel.add(equButton);
+        panel.add(divButton);
+
+
+
+
+        frame.add(panel);
+        frame.add(negButton);
+        frame.add(delButton);
+        frame.add(clrButton);
+        frame.add(textField);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Main calc = new Main();
 /**
  * TO DO
  * map
  * ENUM
  * inheritance (enum) + Override
  */
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        for(int i = 0; i < 10; i++) {
+            if(actionEvent.getSource() == numberButtons[i]) {
+                textField.setText(textField.getText().concat(String.valueOf(i)));
+            }
+        }
+
+        if(actionEvent.getSource() == decButton) {
+            textField.setText(textField.getText().concat("."));
+        }
+        if(actionEvent.getSource() == addButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '+';
+            textField.setText("");
+        }
+        if(actionEvent.getSource() == subButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '-';
+            textField.setText("");
+        }
+        if(actionEvent.getSource() == mulButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '*';
+            textField.setText("");
+        }
+        if(actionEvent.getSource() == divButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '/';
+            textField.setText("");
+        }
+        if(actionEvent.getSource() == equButton) {
+            num2 = Double.parseDouble(textField.getText());
+            switch(operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+            }
+            textField.setText(String.valueOf(result));
+            num1 = result;
+        }
+
+        if (actionEvent.getSource() == clrButton) {
+            textField.setText("");
+        }
+        if (actionEvent.getSource() == delButton) {
+            String temp = textField.getText();
+            textField.setText("");
+            for(int i =0; i < temp.length() -1; i++) {
+                textField.setText(textField.getText() + temp.charAt(i));
+            }
+        }
+        if(actionEvent.getSource() == negButton) {
+            double temp = Double.parseDouble(textField.getText());
+            temp *= -1;
+            textField.setText(String.valueOf(temp));
+        }
     }
 }
